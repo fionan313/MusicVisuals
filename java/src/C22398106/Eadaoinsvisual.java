a3bef20348f321fdb74b8213409a0bfa91a8abda
eadaoin8 <C22398106@mytudublin.ie>
2024-04-11 12:47:27 +0100

    Song now plays, Cases added (Review necessary). Need to add code for cases to display on Screen.

--- java/src/C22398106/Eadaoinsvisual.java
+++ java/src/C22398106/Eadaoinsvisual.java
@@ -1,18 +1,20 @@
 package C22398106;
 
+import ddf.minim.AudioBuffer;
 import ie.tudublin.Visual;
 import ie.tudublin.VisualException;
 
 public class Eadaoinsvisual extends Visual {
+    AudioBuffer ab;
 
     public void settings() {
-        size(1024, 500);
+        //size(1024, 500);
 
         // Use this to make fullscreen
         // fullScreen();
 
         // Use this to make fullscreen and use P3D for 3D graphics
-        // fullScreen(P3D, SPAN);
+        fullScreen(P3D, SPAN);
     }
 
     public void setup() {
@@ -23,7 +25,7 @@ public class Eadaoinsvisual extends Visual {
     }
 
     public void draw() {
-        background(0);
+        background(200);
         try {
             // Call this if you want to use FFT data
             calculateFFT();
@@ -35,5 +37,23 @@ public class Eadaoinsvisual extends Visual {
 
         // Call this is you want to get the average amplitude
         calculateAverageAmplitude();
+
+        float smoothedAmplitude = 0;
+        float average = 0;
+
+        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
+        float cx = width / 2;
+
+        colorMode(HSB);
+        background(200);
+        stroke(0); 
+        for (int i = 0; i < width + 25; i += 25) {
+            for (int j = 0; j < height; j += 25) {
+                float hue = map(i, 0, ab.size() , 0, 256);
+                fill(hue, 255, 255);
+                stroke(15);
+                circle(i, j, cx * smoothedAmplitude);
+            }
+        }
     }
 }
--- java/src/example/MyVisual.java
+++ java/src/example/MyVisual.java
@@ -3,11 +3,23 @@ package example;
 import C22398106.Eadaoinsvisual;
 import C22337521.FionansVisual;
 import C22339066.Alannahsvisual;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+//import processing.core.PApplet;
 import ie.tudublin.*;
 
 public class MyVisual extends Visual {
     WaveForm wf;
     AudioBandsVisual abv;
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    float smooth = 0;
+    int mode = 0;
 
     public void settings() {
         //size(1024, 500);
@@ -20,11 +32,13 @@ public class MyVisual extends Visual {
     }
 
     public void setup() {
-        startMinim();
+        minim = new Minim(this);
         colorMode(HSB,360,255,255);
 
         // Call loadAudio to load an audio file to process
-        loadAudio("Bicep - Glue (Original Mix)_q5rliCxX8xc.mp3");
+        ap = minim.loadFile("Bicep - Glue (Original Mix)_q5rliCxX8xc.mp3");
+        ap.play();
+        ab = ap.mix;
         rectMode(CENTER);
         // Call this instead to read audio from the microphone
         //startListening();
@@ -33,27 +47,45 @@ public class MyVisual extends Visual {
         abv = new AudioBandsVisual(this);
     }
 
-    public void keyPressed() {
-        if (key == ' ') {
-            getAudioPlayer().cue(0);
-            getAudioPlayer().play();
-        }
-        if (key == '1')
-        {
-            String[] a = { "MAIN" };
-            processing.core.PApplet.runSketch(a, new Eadaoinsvisual());
-        }
-        if (key == '2')
-        {
-            String[] a = { "MAIN" };
-            processing.core.PApplet.runSketch(a, new FionansVisual());
-        }
-        if (key == '3')
-        {
-            String[] a = { "MAIN" };
-            processing.core.PApplet.runSketch(a, new Alannahsvisual());
-        }
-    }
+    //public void keyPressed() {
+        //if (key == ' ') {
+        //    getAudioPlayer().cue(0);
+        //    getAudioPlayer().play();
+        //}
+
+        //Éadaoin
+        //if (key == '1')
+        //{
+            //Code here
+
+
+            //ignore
+            /*String[] a = { "MAIN" };
+            processing.core.PApplet.runSketch(a, new Eadaoinsvisual());*/
+        //}
+
+        //Fionán
+        //if (key == '2')
+        //{
+            //Code here
+
+
+            //ignore
+            /*String[] a = { "MAIN" };
+            processing.core.PApplet.runSketch(a, new FionansVisual());*/
+        //}
+
+        //Alannah
+        //if (key == '3')
+        //{
+            //Code here
+
+
+            //ignore
+            /*String[] a = { "MAIN" };
+            processing.core.PApplet.runSketch(a, new Alannahsvisual());*/
+        //}
+    //}
 
     public void draw() {
         background(0);
@@ -71,5 +103,39 @@ public class MyVisual extends Visual {
         calculateAverageAmplitude();
         wf.render();
         abv.render();
+
+        switch(mode)
+        {
+            case 0:
+            {
+                //Add Starter Screen Here
+                //Like say press 1 for ""
+                //Press 2 for ""
+            }
+
+            //Éadaoin's visual
+            case 1:
+            {
+        
+            }
+
+            //Fionán's visual
+            case 2:
+            {
+                
+            }
+
+            //Alannah's visual
+            case 3:
+            {
+                
+            }
+
+            //Group's visual
+            case 4:
+            {
+                
+            }
+        }
     }
 }
