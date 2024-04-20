15eb2a322c2f1890703c841b98381c4927ff721f
Fionán Ó Ceallaigh <fionanoceallaigh@Fionans-Mac-Mini.local>
2024-04-20 15:34:48 +0100

    reated the base code for using .obj files in my Visual.
    Sourced and imported both the .obj file and it's associated .mtl file for a car shape to be used in my first viusal, addition added starter code for using the shape from processing.org

--- java/src/C22337521/FionansVisual.java
+++ java/src/C22337521/FionansVisual.java
@@ -2,24 +2,36 @@ package C22337521;
 
 import ie.tudublin.Visual;
 import ie.tudublin.VisualException;
+import processing.core.PShape;
+
+
 
 public class FionansVisual extends Visual {
 
+    PShape car;
+    float ry;
+
     public void settings() {
-        size(1024, 500);
+        //size(1024, 500);
+        
+        
 
         // Use this to make fullscreen
         // fullScreen();
 
         // Use this to make fullscreen and use P3D for 3D graphics
-        // fullScreen(P3D, SPAN);
+        fullScreen(P3D, SPAN);
     }
 
     public void setup() {
         startMinim();
+        
 
+
+        size(640, 360, P3D);
         // Call loadAudio to load an audio file to process
         loadAudio("Bicep - Glue (Original Mix)_q5rliCxX8xc.mp3");
+        //car = loadShape("sportscar.obj");
     }
 
     public void draw() {
@@ -35,6 +47,17 @@ public class FionansVisual extends Visual {
 
         // Call this is you want to get the average amplitude
         calculateAverageAmplitude();
+
+        lights();
+    
+        translate(width/2, height/2 + 100, -200);
+        rotateZ(PI);
+        rotateY(ry);
+        shape(car);
+        
+        ry += 0.02;
     }
+
+    
     
 }
