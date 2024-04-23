aa4811788f5ae7852c8fa9655411ac6b9fa4cfd8
eadaoin8 <C22398106@mytudublin.ie>
2024-04-23 15:17:47 +0100

    Éadaoin's final edit :)

--- java/src/example/circles.java
+++ java/src/example/circles.java
@@ -19,8 +19,7 @@ public class circles extends poly
         for (int i = 0; i < mv.width + 25; i += 25) {
             for (int j = 0; j < mv.height + 25; j += 25)
             {
-                float hue = (i % 256);
-                //float hue = PApplet.map(i, 0, mv.ab.size(), 0, 256); // Call map on mv (assuming it's a PApplet subclass)
+                float hue = PApplet.map(i, 0, mv.ab.size() + (mv.width/8), 0, 256);
                 mv.fill(hue, 255, 255);
                 mv.stroke(15);
                 mv.circle(i, j, (mv.width / 2) * mv.smooth);
